417221708ace189c5e8866d0b27778e71662930c
ferriyusra <ferisakuragi@gmail.com>
2025-01-20 19:29:47 +0700

    feat: add swagger api docs and add cors

---
+++ src/docs/swagger.ts
@@ -0,0 +1,39 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+	info: {
+		version: 'v0.0.1',
+		title: 'Docs API Acara',
+		description: 'Docs API Acara',
+	},
+	servers: [
+		{
+			url: 'http://localhost:9852/api',
+			description: 'Local server',
+		},
+		{
+			url: 'https://back-end-acara-lac.vercel.app/api',
+			description: 'Development server',
+		},
+	],
+	components: {
+		securitySchemes: {
+			bearerAuth: {
+				type: 'http',
+				scheme: 'bearer',
+			},
+		},
+		schemas: {
+			LoginRequest: {
+				identifier: 'yourname',
+				password: 'yourpassword',
+			},
+		},
+	},
+};
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../routes/api.ts'];
+
+swaggerAutogen({
+	openapi: '3.0.0',
+})(outputFile, endpointsFiles, doc);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/api';
 import db from './utils/database';
+import docs from './docs/route';
+import cors from 'cors';
 
 async function init() {
 	try {
@@ -11,12 +13,16 @@ async function init() {
 		const app = express();
 		const PORT = 9852;
 
+		app.use(cors());
 		app.use(bodyParser.json());
 
 		app.get('/', (req, res) => {
 			res.status(200).json({ message: 'Server is running!', data: null });
 		});
+
 		app.use('/api', router);
+		docs(app);
+
 		app.listen(PORT, () => {
 			console.log(`Server is running on http://localhost:${PORT}`);
 		});
