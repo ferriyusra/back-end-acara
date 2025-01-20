479947f6eff5d03654bd7a003bcccc813ed98e7d
ferriyusra <ferisakuragi@gmail.com>
2025-01-20 15:59:20 +0700

    feat: user register and login api without jwt

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import * as Yup from 'yup';
+import UserModel from '../models/user.model';
+import { encrypt } from '../utils/encryption';
 
 type TRegister = {
 	fullName: string;
@@ -9,12 +11,19 @@ type TRegister = {
 	confirmPassword: string;
 };
 
+type TLogin = {
+	identifier: string;
+	password: string;
+};
+
 const registerValidateSchema = Yup.object({
 	fullName: Yup.string().required(),
 	username: Yup.string().required(),
 	email: Yup.string().email().required(),
 	password: Yup.string().required(),
-	confirmPassword: Yup.string().required(),
+	confirmPassword: Yup.string()
+		.required()
+		.oneOf([Yup.ref('password'), ''], "Password doesn't match"),
 });
 
 export default {
@@ -31,15 +40,63 @@ export default {
 				confirmPassword,
 			});
 
+			const result = await UserModel.create({
+				fullName,
+				email,
+				username,
+				password,
+			});
+
 			res.status(200).json({
 				message: 'User registered successfully',
-				data: {
-					fullName,
-					username,
-					email,
-					password,
-					confirmPassword,
-				},
+				data: result,
+			});
+		} catch (error) {
+			const err = error as unknown as Error;
+			res.status(400).json({
+				message: err.message,
+				data: null,
+			});
+		}
+	},
+
+	async login(req: Request, res: Response) {
+		const { identifier, password } = req.body as unknown as TLogin;
+
+		try {
+			// ambil data user berdasarkkan "identifier" => email dan username
+			const userByIdentifier = await UserModel.findOne({
+				$or: [
+					{
+						email: identifier,
+					},
+					{
+						username: identifier,
+					},
+				],
+			});
+
+			if (!userByIdentifier) {
+				return res.status(403).json({
+					message: 'User not found',
+					data: null,
+				});
+			}
+
+			// validasi password
+			const validatePassword: boolean =
+				encrypt(password) === userByIdentifier.password;
+
+			if (!validatePassword) {
+				return res.status(403).json({
+					message: 'User not found',
+					data: null,
+				});
+			}
+
+			res.status(200).json({
+				message: 'Login success',
+				data: userByIdentifier,
 			});
 		} catch (error) {
 			const err = error as unknown as Error;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/api';
+import db from './utils/database';
 
-const app = express();
+async function init() {
+	try {
+		const result = await db();
+		console.log('database status: ', result);
 
-app.use(bodyParser.json());
+		const app = express();
+		const PORT = 9852;
 
-const PORT = 9852;
+		app.use(bodyParser.json());
+		app.use('/api', router);
+		app.listen(PORT, () => {
+			console.log(`Server is running on http://localhost:${PORT}`);
+		});
+	} catch (error) {
+		console.log('Error Starting Server:', error);
+	}
+}
 
-app.use('/api', router);
-
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-});
+init();
