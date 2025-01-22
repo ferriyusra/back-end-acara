42fecca0ba896b72626d08d8646ca6327a6737b6
ferriyusra <ferisakuragi@gmail.com>
2025-01-22 16:47:01 +0700

    feat: crud api category

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,94 @@
+import { Response } from 'express';
+import { IPaginationQuery, IReqUser } from '../utils/interfaces';
+import CategoryModel, { categoryDAO } from '../models/category.model';
+import response from '../utils/response';
+
+export default {
+	async create(req: IReqUser, res: Response) {
+		try {
+			await categoryDAO.validate(req.body);
+			const result = await CategoryModel.create(req.body);
+			return response.success(res, result, 'Success create category');
+		} catch (error) {
+			return response.error(res, error, 'Failed create category');
+		}
+	},
+	async findAll(req: IReqUser, res: Response) {
+		const {
+			page = 1,
+			limit = 10,
+			search,
+		} = req.query as unknown as IPaginationQuery;
+		try {
+			const query = {};
+
+			if (search) {
+				Object.assign(query, {
+					$or: [
+						{
+							name: { $regex: search, $options: 'i' },
+						},
+						{
+							description: { $regex: search, $options: 'i' },
+						},
+					],
+				});
+			}
+
+			const result = await CategoryModel.find(query)
+				.limit(limit)
+				.skip((page - 1) * limit)
+				.sort({ createdAt: -1 })
+				.exec();
+
+			const count = await CategoryModel.countDocuments(query);
+
+			return response.pagination(
+				res,
+				result,
+				{
+					total: count,
+					totalPages: Math.ceil(count / limit),
+					current: page,
+				},
+				'Success find all category'
+			);
+		} catch (error) {
+			return response.error(res, error, 'Failed find all category');
+		}
+	},
+	async findOne(req: IReqUser, res: Response) {
+		try {
+			const { id } = req.params;
+			const result = await CategoryModel.findById(id);
+
+			if (!result) {
+				return response.error(res, null, 'Category not found');
+			}
+
+			return response.success(res, result, 'Success find one category');
+		} catch (error) {
+			return response.error(res, error, 'Failed find one category');
+		}
+	},
+	async update(req: IReqUser, res: Response) {
+		try {
+			const { id } = req.params;
+			const result = await CategoryModel.findByIdAndUpdate(id, req.body, {
+				new: true,
+			});
+			return response.success(res, result, 'Success update category');
+		} catch (error) {
+			return response.error(res, error, 'Failed update category');
+		}
+	},
+	async remove(req: IReqUser, res: Response) {
+		try {
+			const { id } = req.params;
+			const result = await CategoryModel.findByIdAndDelete(id);
+			return response.success(res, result, 'Success remove category');
+		} catch (error) {
+			return response.error(res, error, 'Failed remove category');
+		}
+	},
+};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,6 +5,7 @@ import aclMiddleware from '../middlewares/acl.middleware';
 import { ROLES } from '../utils/constant';
 import mediaMiddleware from '../middlewares/media.middleware';
 import mediaController from '../controllers/media.controller';
+import categoryController from '../controllers/category.controller';
 
 const router = express.Router();
 
@@ -12,6 +13,24 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 router.get('/auth/me', authMiddleware, authController.me);
 router.post('/auth/activation', authController.activation);
+
+router.post(
+	'/category',
+	[authMiddleware, aclMiddleware([ROLES.ADMIN])],
+	categoryController.create
+);
+router.get('/category', categoryController.findAll);
+router.get('/category/:id', categoryController.findOne);
+router.put(
+	'/category/:id',
+	[authMiddleware, aclMiddleware([ROLES.ADMIN])],
+	categoryController.update
+);
+router.delete(
+	'/category/:id',
+	[authMiddleware, aclMiddleware([ROLES.ADMIN])],
+	categoryController.remove
+);
 
 // router.get(
 // 	'/test-acl',
