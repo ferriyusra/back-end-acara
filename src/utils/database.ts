9493bf5fe6e444eab8ea814788400bc3bc2ad7dd
ferriyusra <ferisakuragi@gmail.com>
2025-01-31 20:23:29 +0700

    refactor db url

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,9 +3,7 @@ import { DATABASE_URL } from './env';
 
 const connect = async () => {
 	try {
-		await mongoose.connect(DATABASE_URL, {
-			dbName: 'db_acara',
-		});
+		await mongoose.connect(DATABASE_URL);
 		return Promise.resolve('Database Connected!');
 	} catch (error) {
 		return Promise.reject(error);
