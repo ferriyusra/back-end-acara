c208844d5cf4d704d8fdc54cbecc3e63323d6eff
ferriyusra <ferisakuragi@gmail.com>
2025-02-04 13:44:18 +0700

    refactor: find all events and api docs

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,13 +1,13 @@
 import { Response } from 'express';
 import { IPaginationQuery, IReqUser } from '../utils/interfaces';
 import response from '../utils/response';
-import EventModel, { eventDAO, TEvent } from '../models/event.model';
+import EventModel, { eventDAO, TypeEvent } from '../models/event.model';
 import { FilterQuery, isValidObjectId } from 'mongoose';
 
 export default {
 	async create(req: IReqUser, res: Response) {
 		try {
-			const payload = { ...req.body, createdBy: req.user?.id } as TEvent;
+			const payload = { ...req.body, createdBy: req.user?.id } as TypeEvent;
 
 			await eventDAO.validate(payload);
 
@@ -20,26 +20,55 @@ export default {
 	},
 	async findAll(req: IReqUser, res: Response) {
 		try {
+			const buildQuery = (filter: any) => {
+				let query: FilterQuery<TypeEvent> = {};
+
+				if (filter.search) {
+					query.$text = {
+						$search: filter.search,
+					};
+				}
+
+				if (filter.category) {
+					query.category = filter.category;
+				}
+
+				if (filter.isOnline) {
+					query.isOnline = filter.isOnline;
+				}
+
+				if (filter.isPublish) {
+					query.isPublish = filter.isPublish;
+				}
+
+				if (filter.isFeatured) {
+					query.isFeatured = filter.isFeatured;
+				}
+
+				return query;
+			};
+
 			const {
 				limit = 10,
 				page = 1,
 				search,
-			} = req.query as unknown as IPaginationQuery;
+				category,
+				isOnline,
+				isFeatured,
+				isPublish,
+			} = req.query;
 
-			const query: FilterQuery<TEvent> = {};
-
-			if (search) {
-				Object.assign(query, {
-					...query,
-					$text: {
-						$search: search,
-					},
-				});
-			}
+			const query = buildQuery({
+				search,
+				category,
+				isPublish,
+				isFeatured,
+				isOnline,
+			});
 
 			const result = await EventModel.find(query)
-				.limit(limit)
-				.skip((page - 1) * limit)
+				.limit(+limit)
+				.skip((+page - 1) * +limit)
 				.sort({ createdAt: -1 })
 				.exec();
 
@@ -49,9 +78,9 @@ export default {
 				res,
 				result,
 				{
-					current: page,
+					current: +page,
 					total: count,
-					totalPages: Math.ceil(count / limit),
+					totalPages: Math.ceil(count / +limit),
 				},
 				'success find all events'
 			);
