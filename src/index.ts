551d7eecdb143124915a22fc81ed1e9c70faf946
ferriyusra <ferisakuragi@gmail.com>
2025-02-11 16:00:04 +0700

    feat: add global error handling

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import router from './routes/api';
 import db from './utils/database';
 import docs from './docs/route';
 import cors from 'cors';
+import errorMiddleware from './middlewares/error.middleware';
 
 async function init() {
 	try {
@@ -22,6 +23,9 @@ async function init() {
 
 		app.use('/api', router);
 		docs(app);
+
+		app.use(errorMiddleware.serverRoute());
+		app.use(errorMiddleware.serverError());
 
 		app.listen(PORT, () => {
 			console.log(`Server is running on http://localhost:${PORT}`);
